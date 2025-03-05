cd7d4ab9e308e4d0b7e845a882849c23a518e30e
EboTheFirst <ebokak5@gmail.com>
2025-03-05 09:21:02 +0000

    Eslint errors fix1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,15 +6,15 @@ const redHatDisplay = Red_Hat_Display({
   subsets: ['latin']
 });
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "Kobi Korankye",
